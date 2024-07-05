89f59363f6b993ccbb71a95af7ffec2f6f285136
DESKTOP-M30FNO0\GA2B <h1202315001@hiratagakuen.onmicrosoft.com>
2024-07-05 16:31:17 +0900

    ほかのリストにcommandリストに合わせた数値をだいにゅうしようとした

--- newGame/Assets/Script/GameManager.cs
+++ newGame/Assets/Script/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public static List<GameObject> CommandList = new List<GameObject>();
-    public List<GameObject> a = new List<GameObject>();
+    public List<ItemName> a = new List<ItemName>();
     public enum GameState
     {
         TITLE,
@@ -42,7 +42,17 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        a = CommandList;
+        for (int i = 0; i < CommandList.Count-1; i++) 
+        {
+            if (CommandList[i].name.StartsWith("Sword"))
+                a[i] = ItemName.Sword;
+            else if (CommandList[i].name.StartsWith("Hammer"))
+                a[i] = ItemName.Hammer;
+            else if (CommandList[i].name.StartsWith("Magic"))
+                a[i] = ItemName.Magic;
+            else if (CommandList[i].name.StartsWith("Potion"))
+                a[i] = ItemName.Potion;
+        }
     }
 
 }
